3fb5bc1b22a7bc095d5ffed99dbc5672c767d476
Dane Wiid <dane.wiid@lavalamp.biz>
2014-03-30 17:14:57 +0200

    error test

--- src/hello/jenkins/HelloJenkins.java
+++ src/hello/jenkins/HelloJenkins.java
@@ -16,7 +16,7 @@ public class HelloJenkins {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins")
     }
     
 }
